e5aa767e617538928282aee0535a14f348d2f8c7
CharlesB91 <charles_ballantyne@hotmail.co.uk>
2021-09-08 16:04:42 +0000

    adjusted case to check if input is integer

--- run.py
+++ run.py
@@ -38,13 +38,12 @@ def validate_data(values):
     Raised ValueError of string cannot be converted into int, 
     or if there arent exactly 6 values.
     """
-    try: 
+    try:
+        [int(value) for value in values] 
         if len(values) != 6:
             raise ValueError(f"Exactly 6 values required, you provided {len(values)}")
     except ValueError as e:
         print(f"Invalid data: {e}, please try again.\n")
-    print(values)
     
-
 
 get_sales_data()
