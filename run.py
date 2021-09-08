b40f7929b5665ec028613186f3e73ace79c7de4c
CharlesB91 <charles_ballantyne@hotmail.co.uk>
2021-09-08 16:16:53 +0000

    added user request loop so that input will be appear until correct data is input

--- run.py
+++ run.py
@@ -20,16 +20,20 @@ def get_sales_data():
     """
     Get Sales Figures  from the user
     """
+    while True:
+        print("Please enter sales data from the last marker")
+        print("Data shold be six numbers, seperated by commas")
+        print("Example: 10,20,30,40,50")
 
-    print("Please enter sales data from the last marker")
-    print("Data shold be six numbers, seperated by commas")
-    print("Example: 10,20,30,40,50")
+        data_str = input("Enter your data here: ")
 
-    data_str = input("Enter your data here: ")
-    print(f"The data provided is {data_str}")
+        sales_data = data_str.split(",")
 
-    sales_data = data_str.split(",")
-    validate_data(sales_data)
+        if validate_data(sales_data):
+            print("Data is valid")
+            break
+
+    return sales_data
 
 
 def validate_data(values):
@@ -44,6 +48,9 @@ def validate_data(values):
             raise ValueError(f"Exactly 6 values required, you provided {len(values)}")
     except ValueError as e:
         print(f"Invalid data: {e}, please try again.\n")
+        return False
+    
+    return True
     
 
-get_sales_data()
+data = get_sales_data()
