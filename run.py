e21b31334aa835c03eb2b219982e40dd14c53727
CharlesB91 <charles_ballantyne@hotmail.co.uk>
2021-09-09 13:43:39 +0000

    code writen to updated predicated stock

--- run.py
+++ run.py
@@ -138,21 +138,37 @@ def get_last_5_entries_sales():
     
     return columns
 
+
+def calculate_stock_data(data):
+    """
+    Calculate the average stock for each item type, adding 10%
+    """
+    print("Calculating the stock data...\n")
+    new_stock_data = []
+
+    for column in data:
+        int_column = [int(num) for num in column]
+        average = sum(int_column) / len(int_column)
+        stock_num = average * 1.1
+        new_stock_data.append(round(stock_num))
+
+    return new_stock_data
+
 ###### THIS BELOW IS AN EXAMPLE OF THE REFACTORED CODE FOR THE ABOVE FUNCTIONS. THE FUNCTION CALLS ARE SLIGHTLY DIFFERENT #########
 
-# def main():
-#     """
-#     Run all program functions
-#     """
-#     data = get_sales_data()
-#     sales_data = [int(num) for num in data]
-#     update_worksheet(sales_data, "sales")
-#     new_surplus_data = calculate_surplus_data(sales_data)
-#     update_worksheet(new_surplus_data, "surplus")
+def main():
+    """
+    Run all program functions
+    """
+    data = get_sales_data()
+    sales_data = [int(num) for num in data]
+    update_worksheet(sales_data, "sales")
+    new_surplus_data = calculate_surplus_data(sales_data)
+    update_worksheet(new_surplus_data, "surplus")
+    sales_columns = get_last_5_entries_sales()
+    stock_data = calculate_stock_data(sales_columns)
+    update_worksheet(stock_data, "stock")
 
 
-# print("Welcome to Love Sandwiches Data Automation")
-# main()
-
-
-sales_columns = get_last_5_entries_sales()
+print("Welcome to Love Sandwiches Data Automation")
+main()
